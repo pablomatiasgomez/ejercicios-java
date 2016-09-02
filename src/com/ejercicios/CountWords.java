bae04dc17dffb1da64d85fc88b996a204db6e60b
Cami <camilabgil@hotmail.com>
2016-09-02 20:47:07 -0300

    Check

--- src/com/ejercicios/CountWords.java
+++ src/com/ejercicios/CountWords.java
@@ -1,4 +1,4 @@
-package com.ejercicios;
+package src.com.ejercicios;
 
 import java.util.Arrays;
 import java.util.HashMap;
--- src/com/ejercicios/GroupFirstLetter.java
+++ src/com/ejercicios/GroupFirstLetter.java
@@ -1,9 +1,11 @@
-package com.ejercicios;
+package src.com.ejercicios;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 // ejercicio:
 // Agrupar por todas las palabras que empiezan con la misma letra
@@ -30,19 +32,30 @@ public class GroupFirstLetter {
         }
     }
 
-    // A mano
-    private static Map<Character, List<String>> groupByFirstLetter1(List<String> words) {
-        // TODO hacer
-        return null;
+	// A mano
+	private static Map<Character, List<String>> groupByFirstLetter1(List<String> words) {
+		Map<Character, List<String>> collectMap = new HashMap<>();
+		words.forEach(word -> {
+			Character firstLetter = word.charAt(0);
+			List<String> equals = collectMap.get(firstLetter);
+			if(equals!=null){
+				equals.add(word);
+			}else{
+				equals = new ArrayList<>();
+				equals.add(word);
+			}
+			collectMap.put(firstLetter, equals);
+		});
+		return collectMap;
+
+	}
+
+	// Stream Api
+	private static Map<Character, List<String>> groupByFirstLetter2(List<String> words) {
+    	return words.stream().collect(Collectors.groupingBy(GroupFirstLetter::getFirstLetter));
     }
 
-    // Stream Api
-    private static Map<Character, List<String>> groupByFirstLetter2(List<String> words) {
-        // TODO hacer
-        return null;
-    }
-
-    private static Character getFirstLetter(String str) {
-        return str.charAt(0);
-    }
+	private static Character getFirstLetter(String str) {
+		return str.charAt(0);
+	}
 }
